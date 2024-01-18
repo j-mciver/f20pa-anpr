e6b967b17dd1ea97f6e618b73235c8493cca0440
John McIver <jm2006@hw.ac.uk>
2024-01-18 15:27:30 +0000

    cv2.imshow retired for matplotlib plotting each process img implemented

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import cv2
 import os
 import time
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 import numpy as np
 
 image_dir = "/Users/jmciver/Documents/Y4S1/F20PA/DISSERTATION-MATERIAL/UKLicencePlateDataset/whiteplate_augmented"
 # image dir must not be hardcoded - make this an embedded project folder potentially on GitHub? + test yellow plates
 image_list = sorted(os.listdir(image_dir))
-
-fig = plt.figure(figsize=(20, 20))
-
-
 
 
 def apply_bilateral_filter(img):
@@ -19,6 +15,8 @@ def apply_bilateral_filter(img):
 
 # reference: bilateral filter algorithm https://docs.opencv.org/4.x/d4/d13/tutorial_py_filtering.html
 IBF_THRESHOLD = 1.00
+
+
 def iterative_bilateral_filter(img):
     fimg = apply_bilateral_filter(img)
     psnr = cv2.PSNR(img, fimg)
@@ -51,11 +49,9 @@ def character_segmentation(th_img):
         if all((5 < w < 50, 45 < h < 65, 500 < area < 1500)):
             componentMask = (labels == i).astype("uint8") * 255
             characters.append(componentMask)
-
             rect_border.append([x, y, w, h])
-            print("RECT_BORDER: ", rect_border)
-
             char_img = cv2.bitwise_or(char_img, componentMask)
+
     return char_img, rect_border, characters
 
 
@@ -71,42 +67,40 @@ def start():
             image_path = image_dir + "/" + file
             image = cv2.imread(image_path)
             greyscale_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            cv2.imshow("converted input RGB image",
-                       greyscale_img)  # todo: make optional to show (on verbose flag enabled)
+            # cv2.imshow("converted input RGB image", greyscale_img)  # todo: make optional to show (on verbose flag enabled)
             # todo: matplotlib for all of them showing inputs and outputs etc
 
             # apply iterative bilateral filter
             filtered_image = iterative_bilateral_filter(greyscale_img)
-            cv2.imshow("Iterative iterative_bilateral_filter Filter", filtered_image)
+            # cv2.imshow("Iterative iterative_bilateral_filter Filter", filtered_image)
 
             # adaptative histogram equalisation
             # AHE reference: https://pyimagesearch.com/2021/02/01/opencv-histogram-equalization-and-adaptive-histogram-equalization-clahe/
             ahe = cv2.createCLAHE(clipLimit=2.2, tileGridSize=(16, 16))
             ahe_img = ahe.apply(filtered_image)
 
-            cv2.imshow("AHE on " + file, ahe_img)
+            # cv2.imshow("AHE on " + file, ahe_img)
 
             # apply otsu's method of automatic thresholding
             # reference: Applying Otsu's method of thresholding. https://docs.opencv.org/3.4/d7/d4d/tutorial_py_thresholding.html
             th_val, th_img = cv2.threshold(ahe_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
             th_img = cv2.bitwise_not(th_img)  # invert binary img OpenCV CCA expects black background, white foreground
             title = "Otsu's Method: " + file + " threshold value: " + str(th_val)
-            cv2.imshow("Input RGB image", image)
-            cv2.imshow(title, th_img)
+            # cv2.imshow("Input RGB image", image)
+            # cv2.imshow(title, th_img)
 
             # Character Segmentation and Extraction
             # Connected-Component Analysis
             char_img, rect_border, characters = character_segmentation(th_img)
-            cv2.imshow("Characters", char_img)
+            # cv2.imshow("Characters", char_img)
 
             output = image.copy()
             # paint rectangle border around all filtered components against original image
             for r in rect_border:
-                print(r)
                 # (x, y), (x + w, y + h)
                 cv2.rectangle(output, (r[0], r[1]), (r[0] + r[2], r[1] + r[3]), (0, 255, 0), 3)
 
-            cv2.imshow("Character Detection (Original Image)", output)
+            # cv2.imshow("Character Detection (Original Image)", output)
 
             # for char in characters:
             #     cv2.imshow("char", char)
@@ -118,13 +112,41 @@ def start():
             print("%s took %s seconds" % (file, time.time() - start_time))
 
             # todo: retire cv2 showwindow and apply into windowed / structured matplotlib instead.
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
-
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
+            #
             count = count + 1
 
-        if count == limit:
-            break
+            """--- DISPLAY PROCESSED IMAGES --- Reference (Display multiple images in matplotlib): 
+            https://www.geeksforgeeks.org/how-to-display-multiple-images-in-one-figure-correctly-in-matplotlib/"""
+            # IF -v (verbose flag enabled) ... show
+            rows = 2
+            cols = 3
+
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # OpenCV reads images BGR, matplotlib reads in RGB. Convert.
+            greyscale_img = cv2.cvtColor(greyscale_img, cv2.COLOR_BGR2RGB)
+            filtered_image = cv2.cvtColor(filtered_image, cv2.COLOR_BGR2RGB)
+            ahe_img = cv2.cvtColor(ahe_img, cv2.COLOR_BGR2RGB)
+            th_img = cv2.cvtColor(th_img, cv2.COLOR_BGR2RGB)
+
+            # average image = 580x160 = 5.3 inches x 1.7
+            fig = plt.figure(figsize=(30, 6))
+
+            i = 1
+            for img, title in [[image, "Input Image " + file], [greyscale_img, "Greyscaled Input RGB Image"],
+                               [filtered_image, "Bilateral Filtered Image"],
+                               [ahe_img, "Adaptive Histogram Equalisation"],
+                               [th_img, "Automatic Thresholding (Otsu's Method)"]]:
+                fig.add_subplot(rows, cols, i)
+                plt.imshow(img)
+                plt.title(title)
+                plt.axis("off")
+                i = i + 1
+
+            plt.show()
+
+            if count == limit:
+                break
 
 
 start()
@@ -136,3 +158,4 @@ start()
 # Otsu's Method of Thresholding (OpenCV): https://docs.opencv.org/3.4/d7/d4d/tutorial_py_thresholding.html
 # https://pyimagesearch.com/2021/02/01/opencv-histogram-equalization-and-adaptive-histogram-equalization-clahe/
 # https://pyimagesearch.com/2021/02/22/opencv-connected-component-labeling-and-analysis/
+# https://www.geeksforgeeks.org/how-to-display-multiple-images-in-one-figure-correctly-in-matplotlib/
