5c692312590d39366d2c20a09375c89102f05bfe
John McIver <jm2006@hw.ac.uk>
2024-02-20 14:21:04 +0000

    reformatted write_results doc comment write_to_xml

--- write_results.py
+++ write_results.py
@@ -1,33 +1,4 @@
 import xml.etree.ElementTree as ET
-import uuid
-
-""""
-    <registration_text> AA10QYN </registration_text>
-    <predicted_text> AA10QYN </predicted_text>
-    <is_correct>true</is_correct>
-    <processing_time_sec> 0.002183sec </processing_time_sec>
-    
-    <max_confidence>
-        <1st_char>90</1st_char>
-        <2nd_char>80</2nd_char>
-                ...
-    </max_confidence>
-    
-    <confidence_distribution>
-        <1>a:90,c:90,d:89,84..</1st_char>
-        <2>a:90,z:29,c:8 ..</2nd>
-        <3>
-        + 0-9 digits
-        .. 7 items * 35 matches in dictionary
-         .. 7 items in every tag (for 7 characters in plate) show distribution for each predicted match
-    </confidence_distribution>
-    <psnr>
-    <degree_of_tilt>
-    <contrast_before_preprocessing>
-    <contrast_after_preprocessing>
-    <brightness> float </brightness>
-    <brightness_category> bright/normal/dark
-"""
 
 global data
 data = []
@@ -47,6 +18,32 @@ def create_child_elements(data, parent):
         i += 1
 
 
+"""" Writes data captures from anpr.py to XML file 
+
+        Example:
+        <registration_text>AA10QYN</registration_text>
+        <predicted_text>AA10QYN</predicted_text>
+        <is_correct>True/False</is_correct>
+        <processing_time_sec>0.002183</processing_time_sec>
+    
+        <max_confidence>
+            <c1>0.90</c1>
+            <c2>0.80</c2>
+                ...
+        </max_confidence>
+    
+        <confidence_distribution>
+            <c1>a:90,c:90,d:89,84..</c1>
+            <c2>a:90,z:29,c:8 ..</c2>
+            .. 7 items * 35 matches in dictionary
+        </confidence_distribution>
+        <psnr>7.613</psnr>
+        <degree_of_tilt>3.6</degree_of_tilt>
+        <contrast_before_preprocessing>30</contrast_before_preprocessing>
+        <contrast_after_preprocessing>90</contrast_after_preprocessing>
+        <brightness>155</brightness>
+        <brightness_category> bright/normal/dark</brightness_category>
+"""
 def write_to_xml_file():
     root = ET.Element('root')
     for entry in data:
@@ -89,7 +86,7 @@ def write_to_xml_file():
         brightness_category.text = entry[11]
 
     tree = ET.ElementTree(root)
-    tree.write(xml_dir + str(uuid.uuid4())[:4] + ".xml")
+    tree.write(xml_dir + "1a_1b_1c_1d_yellowplate_12000_results.xml")
 
 # References:
 # Creating an XMl file using xml.etree: https://docs.python.org/3/library/xml.etree.elementtree.html
